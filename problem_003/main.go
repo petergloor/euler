fbb81393168c4c7e93f61cfbba721ead28c8a26c
Peter Gloor <p.gloor@notesdev.ch>
2017-01-24 21:09:45 +0100

    Added problem 3 and reformatted project

---
+++ problem_003/main.go
@@ -0,0 +1,14 @@
+// project problem_003 main.go
+// Solution for Project Euler Problem #003, Largest prime factor
+package main
+
+func main() {
+
+}
+
+/*
+ Largest prime factor
+ Problem 3
+ The prime factors of 13195 are 5, 7, 13 and 29.
+ What is the largest prime factor of the number 600851475143 ?
+*/
